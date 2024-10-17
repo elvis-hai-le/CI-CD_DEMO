e0af01480b2be71660004cd354cb9fd06d6599b9
Elvis Le <elvishaile120@gmail.com>
2024-10-18 03:27:42 +1100

    Push to deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ ! -d CI-CD_DEMO ]]
+then
+    echo "Cloning from CI-CD_DEMO Git repo"
+    git clone https://github.com/elvis-hai-le/CI-CD_DEMO.git
+    cd /home/ubuntu/CI-CD_DEMO/
+else
+    cd /home/ubuntu/CI-CD_DEMO/
+    git pull origin main
+fi
+npm ci &&
+npm run build &&
+pkill screen
+screen -d -m npm run start 
