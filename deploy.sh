1704c520a7e94bd3f4e0cf3b9f7994a3982c06b2
Elvis Le <elvishaile120@gmail.com>
2024-10-18 04:06:18 +1100

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-if [[ ! -d CI-CD_DEMO ]]
-then
-    echo "Cloning from CI-CD_DEMO Git repo"
-    git clone https://github.com/elvis-hai-le/CI-CD_DEMO.git
-    cd /home/ubuntu/CI-CD_DEMO/
-else
-    cd /home/ubuntu/CI-CD_DEMO/
-    git pull origin main
-fi
+cd /home/ubuntu/CI-CD_DEMO/
+git pull origin main
 npm ci &&
 npm run build &&
 pkill screen
