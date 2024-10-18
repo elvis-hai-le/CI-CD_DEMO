1a439275b0474d167950a5d4de6678d6414bf74f
Elvis Le <elvishaile120@gmail.com>
2024-10-18 20:01:47 +1100

    test

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ echo $PATH
 export PATH=$PATH:/usr/local/bin
 cd /home/ubuntu/CI-CD_DEMO/
 git pull origin main
-npm clean-install &&
+set -x
+npm ci --verbose &&
 npm run build &&
 pm2 restart Snake_app
