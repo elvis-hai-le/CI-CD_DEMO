f2c489b22199ecc33173e06b88086984f6c83f01
Elvis Le <elvishaile120@gmail.com>
2024-10-20 02:28:53 +1100

    improving ci time

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 cd /home/ubuntu/CI-CD_DEMO/
 git pull origin main
-npm ci --verbose &&
+npm npm ci --prefer-offline --no-audit &&
 npm run build &&
 pm2 restart snake
