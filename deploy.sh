7e19267fcacbca290016c13bc997cdee0b400b03
Elvis Le <elvishaile120@gmail.com>
2024-10-20 18:43:22 +1100

    updated workflow to use s3

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Set variables
+reponame=CI-CD_DEMO
+repodir=/home/ubuntu/${reponame}
+webdir=/home/ubuntu/dist
+codedeployname=Snake
+environment=$(cat /etc/nodejs_environment.dat)
+s3bucket=cicd-codeploy-bucket
+
 cd /home/ubuntu/CI-CD_DEMO/
 git pull origin main
+
+#check for build folder and delete if exists
+[ -d $webdir ] && rm -frv ${webdir}/*
+aws s3 sync s3://${s3bucket}/${codedeployname}/${environment}/ ${webdir}/ --delete
+
 bun run install --frozen-lockfile --production &&
-bun run build &&
 pm2 restart snake
